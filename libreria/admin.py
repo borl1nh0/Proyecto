5ef3dd5a720787d2f92d6d463f8d59b243894a2d
Juan Antonio Sancho <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-10-06 13:55:00 +0200

    Modelos Registrados en admin.py

--- libreria/admin.py
+++ libreria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+admin.site.register(Biblioteca)
+admin.site.register(Autor)
+admin.site.register(Libro)
+admin.site.register(Cliente)
+admin.site.register(DatosCliente)
