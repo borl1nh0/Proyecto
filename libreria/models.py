f63fe23a5b78c2255e24a4c954721147afb8aa89
Juan Antonio Sancho <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-10-06 14:44:49 +0200

    BBDD creada

--- libreria/models.py
+++ libreria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,10 +7,20 @@ class Biblioteca(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.TextField()
     
+    
+    def __str__(self):
+        return self.nombre
+    
 class Autor (models.Model):
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=200, blank=True)
     edad = models.IntegerField(null = True)
+    
+    
+    def __str__(self):
+        return self.nombre
+    
+    
 
 class Libro (models.Model):
     IDIOMA = [
@@ -25,15 +36,30 @@ class Libro (models.Model):
         choices=IDIOMA,
         default="ES",
     )
+    autores =   models.ManyToManyField(Autor)
     descripcion = models.TextField()
     fecha_publicacion = models.DateField()
+    Biblioteca = models.ForeignKey(Biblioteca, on_delete= models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.nombre
     
 class Cliente (models.Model):
     nombre = models.CharField(max_length=100)
     email = models.CharField(max_length=200,unique=True)
     puntos = models.FloatField(default=5.0,db_column="puntos_biblioteca")
+    libros = models.ManyToManyField(Libro, through="Prestamo")
+    def __str__(self):
+          return self.nombre
     
 class DatosCliente(models.Model):
     direccion = models.TextField()
     gustos = models.TextField()
     telefono = models.IntegerField()
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE, null=True)
+    
+    
+class Prestamo(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    Libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    fechas_prestamo = models.DateTimeField(default=timezone.now)
